01ba87f3d27fcc8036f87866e1765c62cacb199a
takieddine12 <algeriahero22@gmail.com>
2022-09-19 18:27:16 +0100

    Flutter Notes App

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,5 +1,90 @@
-
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/standalone.dart' as tz;
 
 class NotificationHelper {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin(); //
 
+  initializeNotification() async {
+    tz.initializeTimeZones();
+
+    // FOR IOS , at the initialization , we won't ask for permission
+    final initializationSettingsIOS = DarwinInitializationSettings(
+        requestSoundPermission: false,
+        requestBadgePermission: false,
+        requestAlertPermission: false,
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification
+    );
+
+    const initializationSettingsAndroid = AndroidInitializationSettings("appicon");
+
+    final InitializationSettings initializationSettings = InitializationSettings(
+      iOS: initializationSettingsIOS,
+      android: initializationSettingsAndroid,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  }
+
+  // FOR OLDER IOS VERSIONS
+  Future onDidReceiveLocalNotification(int id, String? title, String? body, String? payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    Get.dialog(const Text('Welcome To Flutter'));
+  }
+
+  // WHEN CLIKING ON THE NOTIFICATION
+  Future selectNotification(String? payLoad) async {
+    if(payLoad != null){
+      print('Notification Payload $payLoad');
+    } else {
+      print('Notification Done');
+    }
+    Get.to(() => Container(color: Colors.white));
+  }
+
+  // REQUEST IOS PERMISSIONS AFTER INITIALIZATION
+  void requestIOSPermissions() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+        IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+
+  // Display Notification
+  displayNotification({required String title, required String body}) async {
+    print("doing test");
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        'your channel id', 'your channel name',importance: Importance.max, priority: Priority.high);
+    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'It could be anything you pass',
+    );
+  }
+  // Schedule Notification
+  scheduledNotification() async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'scheduled title',
+        'theme changes 5 seconds ago',
+        tz.TZDateTime.now(tz.getLocation('Europe/Paris')).add(const Duration(seconds: 5)),
+        const NotificationDetails(
+            android: AndroidNotificationDetails('your channel id', 'your channel name',)),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime);
+
+  }
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:notes_app/services/notification_services.dart';
 import 'package:notes_app/services/theme_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +14,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late NotificationHelper notificationHelper;
+  @override
+  void initState() {
+    super.initState();
+    notificationHelper = NotificationHelper();
+    notificationHelper.initializeNotification();
+    notificationHelper.requestIOSPermissions();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +37,15 @@ class _HomePageState extends State<HomePage> {
       leading: GestureDetector(
         onTap: (){
           print('Tapped');
+
+          // Switch App Theme
           ThemeService().switchTheme();
+          // Display Notification To User
+          notificationHelper.displayNotification(
+              title: 'Simple Notification',
+              body: Get.isDarkMode ? 'Activated Dark Mode' : 'Activated Light Mode');
+
+          // Schedule Notification For Certain interval To Show
         },
         child: const Icon(Icons.nightlight_round,size: 20,),
       ),
@@ -37,4 +56,6 @@ class _HomePageState extends State<HomePage> {
       ],
     );
   }
+
+
 }
